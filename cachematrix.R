50e1cfc5b504d9588b533940708d2775364e660b
mike oneill <mdoneill90@gmail.com>
2024-01-15 12:21:43 -0500

    Peer Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Instead of x being numeric I changed it to be a matrix
+## and changed m to z and set it as NULL. I also changed all references of 
+##"mean" to "solve" 
 
 makeCacheMatrix <- function(x = matrix()) {
+    z <- NULL
+    set <- function(y){
+      x <<- y
+      z <<- NULL
+    }
 
+    get <- function() x
+    setInverse <- function(solveMatrix) z <<- solveMatrix
+    getInverse <- function() z
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## I did the same thing again changed "m" to "z" and "mean" to "solve"
 
 cacheSolve <- function(x, ...) {
+  z <- x$getInverse()
+  if(!is.null(z)){
+    message("getting cached data")
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data)
+  x$setInverse(z)
+  z      
         ## Return a matrix that is the inverse of 'x'
 }
